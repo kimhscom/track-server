471f71e5e6a2aa452d9798b3429c8f9d93c7a46b
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-13 16:36:06 +0900

    #174 Connecting to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
