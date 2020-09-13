6b775195f11184101f2cdb354ab2ab72e2b17e0d
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-13 16:51:59 +0900

    #176 Understanding the Signup Process

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(authRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
